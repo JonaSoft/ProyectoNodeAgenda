6c2ade1868ae1383b8524b9e0079794091f0e02e
CASA-PORTATIL\Casa <carlosucananbarrera@hotmail.com>
2018-08-31 08:32:54 -0500

    Actualizando soporte

--- server/insertarUsuario.js
+++ server/insertarUsuario.js
@@ -5,17 +5,18 @@ module.exports.insertarRegistro = function (db, callback){
 			{id:1,nombre:"Carlos Barrera", email:"carlos_b@gmail.com",psw:"12345"}
 		]);
 	var myobj1 =([
-			{id:1,titulo:"Visita a padres", allDay:false,start:"2018-09-20",end:"2018-09-21",email:"carlos_b@gmail.com"}
+			{id:1,titulo:"Visita a padres", allDay:false,start:"2018-09-20",end:"2018-09-20",email:"carlos_b@gmail.com"},
+			{id:2,titulo:"Paseo al Parque", allDay:false,start:"2018-09-24",end:"2018-09-24",email:"carlos_b@gmail.com"}
 		]);
 
 	dbo.collection("usuarios").insert(myobj, function(err,res){
 		if (err) throw err;
-			console.log("Resultado: "+ myobj.length +" usuario adicionado");
+			console.log("Resultado: "+ myobj.length +" usuario(s) adicionado(s)");
 	
 	});
 	dbo.collection("eventos").insert(myobj1, function(err,res){
 		if (err) throw err;
-			console.log("Resultado: "+ myobj.length +" evento adicionado");
+			console.log("Resultado: "+ myobj1.length +" evento(s) adicionado(s)");
 			console.log("Ahora invoca por consola al index.js. Adios...")
 	})  
 }
